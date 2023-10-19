a8e3574239b2a836401c49156d8580d2ca0075f3
Edward Rana <82434266+edward-rana@users.noreply.github.com>
2023-10-19 23:18:41 +0530

    Delete build/php directory

--- build/php/theme/404.js
+++
@@ -1,6 +0,0 @@
-html += ``+ render('navbar.topbar') +`
-
-<div class="container-fluid p-5 bg-light text-dark text-center mt-3">
-    <h1>404 not found</h1>
-    <a href="#"><i class="fa fa-home"> Homepage</i></a>
-</div>`;
